6318e7c0f9278a1d2c95fef7e10475edb721b3e9
trlkly <trlkly@hotmail.com>
2018-01-13 03:01:44 -0600

    Simplified GM4 script
    Hopefully possible to inline and made pre-ES6 compatible

---
+++ assets/js/gm4-shim.js
@@ -0,0 +1,50 @@
+/*
+Functions copied from https://gist.github.com/arantius/3123124
+The MIT License (MIT)
+
+Copyright (c) 2014 Anthony Lieuallen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+*/
+
+if (typeof GM !== 'undefined') {
+	var GM_xmlhttpRequest = GM.xmlHttpRequest;
+    var storagePrefix = GM.info.script.namespace + '?';
+
+	// The following functions all use local storage, and thus could be accessed
+	// by the host. They are also restricted to a single domain.
+
+	var GM_deleteValue = function (aKey) {
+		localStorage.removeItem(storagePrefix + aKey);
+	};
+
+	var GM_getValue = function (aKey, aDefault) {
+		var aValue = localStorage.getItem(storagePrefix + aKey);
+		if (null === aValue && 'undefined' !== typeof aDefault) return aDefault;
+		return aValue;
+	};
+
+	var GM_listValues = function () {
+		var prefixLen = storagePrefix.length;
+		var values = [];
+		for (var i = 0; i < localStorage.length; i++) {
+			var k = localStorage.key(i);
+			if (k.substr(0, prefixLen) === storagePrefix) {
+				values.push(k.substr(prefixLen));
+			}
+		}
+		return values;
+	};
+
+	var GM_setValue = function (aKey, aVal) {
+		localStorage.setItem(storagePrefix + aKey, aVal);
+	};
+}
