46a5e1ab0e58df8069fae3053d704393e3f8eca2
Alexander Krivács Schrøder <alexschrod@gmail.com>
2019-03-06 08:10:24 +0100

    Update copyright year

--- licenseBanner.js
+++ licenseBanner.js
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2016-2018 Alexander Krivács Schrøder <alexschrod@gmail.com>
+ * Copyright (C) 2016-2019 Alexander Krivács Schrøder <alexschrod@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@
 
 const licenseBanner =
 `/* 
- * Copyright (C) 2016-2018 Alexander Krivács Schrøder <alexschrod@gmail.com>
+ * Copyright (C) 2016-2019 Alexander Krivács Schrøder <alexschrod@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
